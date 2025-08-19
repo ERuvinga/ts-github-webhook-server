a3699464a06e5ae6229381c380cd671f984e99c3
ERuvinga <ruvingaelie@gmail.com>
2025-08-19 14:58:11 +0200

    feat : adding external executable script

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,80 @@
 import express = require("express");
+import crypto = require("crypto");
+import childs_process = require("child_process");
+import Util = require("util");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
+const SECRET = "Jeanine-Banza_vps_secret_key";
+const SCRIPT_PATH = '/home/eruvinga/Desktop/work/Jeanine/api'
+
+// change exec function to async function
+const execAsync = Util?.promisify(childs_process?.exec);
+
+// Type to body
+declare global {
+  namespace Express {
+    interface Request {
+      rawBody?: Buffer;
+    }
+  }
+}
 
 // Middleware JSON
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req: any, res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
 
+//function 
+async function deploy() {
+  try {
+    const { stdout, stderr } = await execAsync(
+      `bash ${SCRIPT_PATH}/auto_deploy.sh`
+    );
+    console.log("STDOUT:", stdout);
+    console.error("STDERR:", stderr);
+  } catch (err) {
+    console.error("Erreur déploiement:", err);
+  }
+}
+
+
+// test Route
 app.get("/", (req, res) => {
   res.send("🚀 webhook github server test!");
 });
 
+//web Hooks endpoint
+app.post("/webhook", (req, res) => {
+  // get Signature in Headers Querry
+  const signature = req.headers["x-hub-signature-256"] as string;
+  if (!signature || !req.rawBody)
+    return res.status(400).send("Signature manquante");
+
+  // get Signature
+  const expectedSignature =
+    "sha256=" +
+    crypto.createHmac("sha256", SECRET).update(req.rawBody).digest("hex");
+
+  // compare signatures
+  if (signature !== expectedSignature) {
+    return res.status(401).send("Signature invalide");
+  }
+
+  //Execute script
+
+  console.log("✅ Webhook vérifié, payload:", req.body);
+  //Deploy App
+
+  deploy();
+  res.send("ok");
+});
+
+// listen server datas
 app.listen(PORT, () => {
   console.log(`✅ server started to http://localhost:${PORT}`);
 });
